380d037f49f71923fc3b48bcd5dd918d94a18028
Santiago Neira <santiagoneira52@gmail.com>
2025-11-09 22:36:08 -0300

    Cargar comprobante de pago al envio

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
-// export const API_CESAR =
-//   "https://apicadeteria-i0sonygy0-cesars-projects-2539e6a6.vercel.app";
+export const API_CESAR =
+  "https://apicadeteria-i0sonygy0-cesars-projects-2539e6a6.vercel.app";
 export const API_SANTI =
   "https://apicadeteria-oiruptoqe-sanei1509s-projects.vercel.app";
 
-export const API_CESAR = "http://localhost:3006";
+// export const API_CESAR = "http://localhost:3006";
