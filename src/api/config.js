ec2c8cda3f94ee341fa0f396a91ab2b1fcc6f428
Santiago Neira <santiagoneira52@gmail.com>
2025-11-10 17:59:20 -0300

    Cambio de la URL api

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,6 @@
 export const API_CESAR =
-  "https://apicadeteria-i0sonygy0-cesars-projects-2539e6a6.vercel.app";
+  "https://apicadeteria-rgx0pez2o-cesars-projects-2539e6a6.vercel.app";
+
 export const API_SANTI =
   "https://apicadeteria-oiruptoqe-sanei1509s-projects.vercel.app";
 
