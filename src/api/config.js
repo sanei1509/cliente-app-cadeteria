fe4c7c65c8e2365310254ea6dae49bfaa68b7d82
César <cesar.martinez@docente.ceibal.edu.uy>
2025-11-12 12:46:46 -0300

    cambio de url api orque apuntaba a local en el despliegue

--- src/api/config.js
+++ src/api/config.js
@@ -1,6 +1,6 @@
-// export const API_CESAR = "https://apicadeteria-20en4sky0-cesars-projects-2539e6a6.vercel.app";
+export const API_CESAR = "https://apicadeteria-20en4sky0-cesars-projects-2539e6a6.vercel.app";
 
 export const API_SANTI =
   "https://apicadeteria-oiruptoqe-sanei1509s-projects.vercel.app";
 
-export const API_CESAR = "http://localhost:3006";
+//export const API_CESAR = "http://localhost:3006";
